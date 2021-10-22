b6f283419c7a216f35078129121e2f8855ba4bab
hüseyin can soylu <hsyncn17@gmail.com>
2021-10-23 00:34:50 +0300

    added custom error middleware

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ require("dotenv").config();
 
 const tasksRouter = require("./routes/tasks");
 
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 //middleware
 app.use(express.json());
 
 const PORT = process.env.PORT || 7070;
 
 app.use("/api/v1/tasks", tasksRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
